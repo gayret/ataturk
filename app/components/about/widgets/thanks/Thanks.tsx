1fe9ac043c546048aaf47c291a5060c55f055d91
Safa Gayret <safagayret@gmail.com>
2025-08-26 13:25:55 +0300

    feat: add new thanks

--- app/components/about/widgets/thanks/Thanks.tsx
+++ app/components/about/widgets/thanks/Thanks.tsx
@@ -1,4 +1,5 @@
 import styles from './Thanks.module.css'
+import Link from 'next/link'
 
 export default function Thanks() {
   return (
@@ -7,6 +8,14 @@ export default function Thanks() {
       <ul className={styles.thanksList}>
         <li className={styles.listItem}>
           İrem Çiftler Gayret <i className={styles.detail}>(Kullanıcı testleri ve analiz)</i>
+        </li>
+        <li className={styles.listItem}>
+          <Link
+            href='https://www.linkedin.com/in/osman-emre-hac%C4%B1arap-a1182a23b'
+            target='_blank'
+          >
+            Osman Emre Hacıarap <i className={styles.detail}>(Kullanıcı testleri ve analiz)</i>
+          </Link>
         </li>
       </ul>
     </div>
