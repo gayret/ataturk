c0b03f43a4240dc6c6db1d3ddd085247715a89e5
Safa Gayret <safagayret@gmail.com>
2025-09-02 08:07:00 +0000

    fix: #41 update header link to include 'about' query parameter

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -7,7 +7,7 @@ import Search from './widgets/Search'
 export default function Header() {
   return (
     <header className={styles.header}>
-      <Link href='/' className={styles.link}>
+      <Link href='/?id=about' className={styles.link}>
         <Image className={styles.logo} src={HeaderPicture} alt='logo' width={200} height={200} />
         <h1 className={styles.title}>Atatürk Kronolojisi</h1>
       </Link>
--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,6 @@ import Content from './components/content/Content'
 export default function Home() {
   const events = useEventsData()
   const searchParams = useSearchParams()
-
   const MapWithNoSSR = useMemo(
     () =>
       dynamic(() => import('@/app/components/map/Map'), {
@@ -21,26 +20,41 @@ export default function Home() {
     []
   )
 
-  return searchParams.get('id') === null ? (
-    <>
-      <Header />
-      <Timeline />
-      <About />
-    </>
-  ) : (
-    <>
-      <MapWithNoSSR
-        location={
-          events.find((item) => item.id === Number(searchParams.get('id')))?.location || {
-            lat: 0,
-            lon: 0,
-          }
-        }
-      />
+  // Eğer id yoksa, searchParams'a id=1 ekle
+  if (typeof window !== 'undefined' && searchParams.get('id') === null) {
+    const params = new URLSearchParams(window.location.search)
+    params.set('id', '1')
+    window.location.search = params.toString()
+    return null
+  }
 
-      <Header />
-      <Content />
-      <Timeline />
-    </>
-  )
+  // Eğer searchParams 'about' ise
+  if (searchParams.get('id') === 'about') {
+    return (
+      <>
+        <Header />
+        <Timeline />
+        <About />
+      </>
+    )
+  }
+
+  // Eğer searchParams 'about' değilse ve null değilse, normal render et
+  if (searchParams.get('id') !== 'about' && searchParams.get('id') !== null) {
+    return (
+      <>
+        <MapWithNoSSR
+          location={
+            events.find((item) => item.id === Number(searchParams.get('id')))?.location || {
+              lat: 0,
+              lon: 0,
+            }
+          }
+        />
+        <Header />
+        <Content />
+        <Timeline />
+      </>
+    )
+  }
 }
