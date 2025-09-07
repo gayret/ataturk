f2614ea27b6f38e424d4469a73ee5167260a5ead
Safa Gayret <safagayret@gmail.com>
2025-09-07 20:15:33 +0300

    Update Thanks.tsx

--- app/components/about/widgets/thanks/Thanks.tsx
+++ app/components/about/widgets/thanks/Thanks.tsx
@@ -7,19 +7,11 @@ export default function Thanks() {
       <h2 className={styles.title}>Emek verenler</h2>
       <ul className={styles.thanksList}>
         <li className={styles.listItem}>
-          İrem Çiftler Gayret <i className={styles.detail}>(Kullanıcı testleri ve analiz)</i>
-        </li>
-        <li className={styles.listItem}>
           <Link
             href='https://www.linkedin.com/in/osman-emre-hac%C4%B1arap-a1182a23b'
             target='_blank'
           >
             Osman Emre Hacıarap <i className={styles.detail}>(Seslendirme Yönetmeni)</i>
-          </Link>
-        </li>
-        <li className={styles.listItem}>
-          <Link href='https://marasavucumda.com' target='_blank'>
-            Yusuf Köleli <i className={styles.detail}>(Konsept Danışmanı)</i>
           </Link>
         </li>
         <li className={styles.listItem}>
