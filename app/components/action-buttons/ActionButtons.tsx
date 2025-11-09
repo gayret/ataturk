f1af4b3fd4982df314addd5fb0a87244666f7561
omer <odabasieyupomer@gmail.com>
2025-11-09 16:07:16 +0300

    768px altina hamburger iconu eklendi.

--- app/components/action-buttons/ActionButtons.tsx
+++ app/components/action-buttons/ActionButtons.tsx
@@ -9,10 +9,16 @@ import EventFilter from './widgets/event-filter/EventFilter'
 import Search from './widgets/search/Search'
 import AutoPlay from './widgets/auto-play/AutoPlay'
 import LanguageSelector from './widgets/language-selector/LanguageSelector'
+import { useState } from 'react'
 
-export default function ActionButtons({ showOnlyLanguageSelector = false }: { showOnlyLanguageSelector?: boolean }) {
+export default function ActionButtons({
+  showOnlyLanguageSelector = false,
+}: {
+  showOnlyLanguageSelector?: boolean
+}) {
   const searchParams = useSearchParams()
   const events = useEventsData()
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
 
   const selectedItem =
     events.find((item: ItemType) => item.id === Number(searchParams.get('id'))) || events[0]
@@ -27,8 +33,8 @@ export default function ActionButtons({ showOnlyLanguageSelector = false }: { sh
 
   return (
     <div className={styles.actionButtons}>
-      {
-        selectedItem && (
+      <div className={`${styles.buttonsWrapper} ${isMenuOpen ? styles.open : ''}`}>
+        {selectedItem && (
           <>
             <StreetView lat={selectedItem?.location.lat} lon={selectedItem?.location.lon} />
 
@@ -45,6 +51,16 @@ export default function ActionButtons({ showOnlyLanguageSelector = false }: { sh
             <LanguageSelector />
           </>
         )}
+      </div>
+      <div
+        className={styles.hamburgerButton}
+        onClick={() => setIsMenuOpen(!isMenuOpen)}
+        aria-label='Menüyü aç/kapat'
+      >
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
     </div>
   )
 }
