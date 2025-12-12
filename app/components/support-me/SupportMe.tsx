9d4baafe417164a352b70287456b4eea4048ca2a
Safa Gayret <safagayret@gmail.com>
2025-12-12 15:22:42 +0300

    update: support button

--- app/components/support-me/SupportMe.tsx
+++ app/components/support-me/SupportMe.tsx
@@ -1,19 +1,17 @@
 import styles from './SupportMe.module.css'
-import Image from 'next/image'
 import Link from 'next/link'
-import bookImage from '../../assets/images/kitap.jpeg'
 import { useLanguageStore } from '@/app/stores/languageStore'
 
 export default function SupportMe() {
   const { t } = useLanguageStore()
+
   return (
     <Link
       target='_blank'
-      href='https://www.kitapyurdu.com/kitap/erken-bulunmus-bir-intihar-mektubu-/561410.html'
-      className={styles.book}
+      href='https://buymeacoffee.com/safagayret'
+      className={styles.supportButton}
     >
-      <span className={styles.description}>{t.SupportMe.description}</span>
-      <Image src={bookImage} width={200} alt={t.SupportMe.imageAlt} />
+      {t.SupportMe.description} 🤝
     </Link>
   )
 }
